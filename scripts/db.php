eb6cd2e682b112d8a1d7487a55efe17c831b3f13
Filip Bernhard <filipbarrdal@gmail.com>
2026-07-22 14:43:37 +0200

    Added gitignore and env files

--- scripts/db.php
+++ scripts/db.php
@@ -1,3 +1,9 @@
 <?php 
-    $db = mysqli_connect("localhost","root","","resale_site");
+    $env = parse_ini_file(__DIR__ . '/../.env');
+
+    $db = mysqli_connect($env["DB_HOST"],$env["DB_USER"],$env["DB_PASS"],$env["DB_NAME"]);
+
+    if(!$db){
+        die("Connection failed " . mysqli_connection_error());
+    }
 ?>
