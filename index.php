f18c19bec76e4ef8a81a04afab77a7eeaf286ef0
Debian <debian@vps-4e9150e8.vps.ovh.net>
2026-09-24 13:54:50 +0000

    Added root as an environmental variable. So now it should be more dynamic to pull and use from anywhere

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php include "scripts/header.php" ?>
     <main class="flex flex-col min-h-[90lvh]">
         <?php include "scripts/selectPost.php"; 
-
-            if($_SESSION["loggedIn"] == true){
-            echo '
-            <h3 class="ml-5 mt-2 border-b-1 w-75 text-3xl italic ">Your listings</h3>
-            <section class="index-section">';
-
-              selectUserPosts($_SESSION["id"]);  
-            }
-            else{
-            echo '
+           echo '
             <h3 class="ml-5 mt-2 border-b-1 w-75 text-3xl italic ">Listings</h3>
             <section class="index-section">';
               selectAllPosts(); 
-            }
         ?>
         </section>
 
